8e245c573d195b2c9dab2b365eac276e8d554a9a
SamGarner <samuel.w.garner@gmail.com>
2020-07-21 18:32:25 -0500

    clean up old comments

--- caesar_cipher_testing.rb
+++ caesar_cipher_testing.rb
@@ -3,36 +3,14 @@ phrase = gets.chomp.to_s
 print "please enter a number to use as the shift factor:"
 shift = gets.chomp.to_i
 
-print "phrase: "
-p phrase
-print "shift: "
-p shift
+# print "phrase: "
+# p phrase
+# print "shift: "
+# p shift
 
-#first character, no a-z wrap:
-
-# alphabet  = ('a'..'z').to_a
-# character = phrase[0]
- new_string_elements = []
-#p character
-
-# if contains to make sure letter
-
-# placement = alphabet.index(character)
-# new_placement = placement + shift
-# new_string_elements.push(alphabet[new_placement])
-# new_string = new_string_elements.join()
-
-
-# multiple characters
+new_string_elements = []
 alphabet  = ('a'..'z').to_a
 character_split = phrase.split("")
-
-##### if placement + shift > 26 (involve modulo somehow for e.g. user input = 100)
-##### subtract then and start from zero orrrr just -26 the approp # of times.
-##### subtract 26 until new_placement < 26
-
-### divide new_placement by 26 (integer) and subtract 26*result from new placement
-### then shift
 
 character_split.each do |character|
 	if character.match(/\p{Lower}/) && alphabet.include?(character)
