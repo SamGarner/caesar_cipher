df560d24770b5d974c3c339c97545c2cfea7b09c
SamGarner <samuel.w.garner@gmail.com>
2020-07-22 14:46:03 -0500

    Update user input phrasing and add second file/version

--- caesar_cipher_testing_2.rb
+++ caesar_cipher_testing_2.rb
@@ -8,17 +8,17 @@ alphabet  = ('a'..'z').to_a
 character_split = phrase.split("")
 
 character_split.each do |character|
-	if character.match(/\p{Lower}/) #&& alphabet.include?(character)
-		placement = alphabet.index(character)
-		new_placement = placement + shift
+
+	if alphabet.include?(character.downcase)
+		placement = alphabet.index(character.downcase)
+		new_placement = placement + shift 
 		number_of_wraps = new_placement/26
 		final_placement = new_placement-(26*number_of_wraps)
+	end
+
+	if character.match(/\p{Lower}/) #&& alphabet.include?(character)
 		new_string_elements.push(alphabet[final_placement])
 	elsif character.match(/\p{Upper}/) #&& alphabet.include?(character.downcase)
-		placement = alphabet.index(character.downcase)
-		new_placement = placement + shift
-		number_of_wraps = new_placement/26
-		final_placement = new_placement-(26*number_of_wraps)
 		new_string_elements.push(alphabet[final_placement].upcase)
 	else
 		new_string_elements.push(character)
